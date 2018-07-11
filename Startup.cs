3b0493d5cedeb7bf3575d53e9171bd3b38598439
gradampl <gr_adam@o2.pl>
2018-07-11 21:52:03 +0200

    connect to SQLDB in Azure

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,21 @@ namespace WebApplication1
             {
                 configuration.RootPath = "ClientApp/dist";
             });
-        }
+
+
+            // Use SQL Database if in Azure, otherwise, use SQLite
+if(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
+    services.AddDbContext<MyDatabaseContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("MyDbConnection")));
+else
+    services.AddDbContext<MyDatabaseContext>(options =>
+            options.UseSqlite("Data Source=localdatabase.db"));
+
+// Automatically perform database migration
+services.BuildServiceProvider().GetService<MyDatabaseContext>().Database.Migrate();
+        
+
+         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
