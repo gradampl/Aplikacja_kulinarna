e3691d62541079cf1abd8253441b0503539e1f73
gradampl <gr_adam@o2.pl>
2018-07-13 19:06:33 +0200

    connect to SQLDB in Azure

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,11 @@
+using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
